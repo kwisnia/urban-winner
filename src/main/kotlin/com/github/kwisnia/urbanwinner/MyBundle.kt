f9c219a83c8ec61582d53f0f795bad5a0ca8c6c7
GitHub Action <action@github.com>
2022-03-12 15:16:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/kwisnia/urbanwinner/MyBundle.kt
+++ src/main/kotlin/com/github/kwisnia/urbanwinner/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kwisnia.urbanwinner
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kwisnia/urbanwinner/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kwisnia/urbanwinner/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kwisnia.urbanwinner.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kwisnia.urbanwinner.MyBundle
 
 class MyApplicationService {
 
